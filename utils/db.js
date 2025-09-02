4563bf5fd96b5b5486816d8ba64a0efd4a94adba
kshakya154 <kshakya154@gmail.com>
2025-09-02 14:55:55 +0530

    Add MongoDB URI to environment variables and update database connection to use it

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 async function connectDB(){
   try {
-    await mongoose.connect('mongodb+srv://kshakya154:9855989810@cluster0.kluuyhp.mongodb.net/')
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("Db connected Succesfully");
   } catch (error) {
     console.log("error while connection to the mongo db")
