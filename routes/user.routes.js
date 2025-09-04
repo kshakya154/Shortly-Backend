e8e94bd9960bdee27f5715993ba317859761ab70
kshakya154 <kshakya154@gmail.com>
2025-09-04 11:42:00 +0530

    Refactor user routes for improved code consistency and clarity

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,15 @@
-import express from "express"
-import { signup,login } from "../controllers/user.controller.js";
+import express from "express";
+import { signup, login } from "../controllers/user.controller.js";
 
-const userRoutes=express.Router();
-userRoutes.post('/signup',signup);
-userRoutes.post('/login',login);
-userRoutes.post('/logout', (req, res) => {
+const userRoutes = express.Router();
+userRoutes.post("/signup", signup);
+userRoutes.post("/login", login);
+userRoutes.post("/logout", (req, res) => {
   // Clear the token cookie
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // only over HTTPS in prod
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
   });
   res.status(200).json({ message: "Logged out successfully" });
 });
